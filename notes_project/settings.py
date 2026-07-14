9e874b50d2fe709a3f5397b8bced577e67873014
sonu77640 <kumarsonukumar7760@gmail.com>
2026-07-14 14:42:46 +0530

    fix static

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -140,9 +140,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
